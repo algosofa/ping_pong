94e8950916e13eaa64d20cde8091634713788c0b
algosofa <157206943+algosofa@users.noreply.github.com>
2024-01-27 17:15:27 +0700

    Update pingpong.py
    final ver. no bugs

--- pingpong.py
+++ pingpong.py
@@ -62,15 +62,24 @@ while game:
         ball.rect.x += speed_x
         ball.rect.y += speed_y
 
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= -1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+        
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200,200))
+        
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200,200))
 
         racket1.reset()
         racket2.reset()
         ball.reset()
-    
-    display.update()
-    clock.tick(60)
-    if finish != True:
-        window.fill(background)
     
     display.update()
     clock.tick(60)
